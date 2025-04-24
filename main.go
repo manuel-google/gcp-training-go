b05e57cb3c352f8e1d1bd6b8d6783527ff1928c8
manuel-google <manueledwards@google.com>
2025-04-24 17:17:22 +0000

    adnother commit

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ func main() {
 		// Get the target from the query parameter, default to "World"
 		target := r.URL.Query().Get("target")
 		if target == "" {
-			target = "World 3"
+			target = "World 4"
 		}
 
 		log.Printf("Serving request for path: %s", r.URL.Path) // Log request path
